920cf3f7fd7089203479c26abad44ccf330f06de
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-04-16 18:44:08 -0500

    movie items component added

---
+++ src/components/MovieItems/MovieItems.jsx
@@ -0,0 +1,36 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+const MovieItems = ({ movie }) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleEdit = () => {
+    history.push(`/edit/${movie.id}`);
+    dispatch({ type: "CLEAR_MOVIE_DETAILS" });
+  };
+
+  return (
+    <div className="movie-card">
+      <Card sx={{ minWidth: 300, padding: 2 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 20 }} gutterBottom>
+            {movie.title}
+          </Typography>
+        </CardContent>
+        <img
+          onClick={() => history.push(`/details/${movie.id}`)}
+          className="poster"
+          src={movie.poster}
+          alt={movie.title}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default MovieItems;
