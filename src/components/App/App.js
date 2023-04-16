a0373e589f4879050cde389477647f053cc5f959
Sean Kimbrough <114960881+SeanKimbrough@users.noreply.github.com>
2023-04-16 18:38:38 -0500

    movie details component added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,35 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
-import './App.css';
-import MovieList from '../MovieList/MovieList'
+import { HashRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import MovieList from "../MovieList/MovieList";
+import MovieDetails from "../MovieDetails/MovieDetails";
+import MovieForm from "../MovieForm/MovieForm";
+import NavBar from "../Navbar/Navbar";
+import EditPage from "../EditPage/EditPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>        
+      <NavBar />
+      <Router>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
-        {/* Details page */}
 
-        {/* Add Movie page */}
+        {/* Details page */}
+        <Route path="/details/:id">
+          <MovieDetails />
+        </Route>
+        {/* Add Movie Page */}
+        <Route path="/addMovie">
+          <MovieForm />
+        </Route>
+        {/* Edit page */}
+        <Route path="/edit/:id">
+          <EditPage />
+        </Route>
       </Router>
     </div>
   );
 }
-
 
 export default App;
---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,68 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+const MovieDetails = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const movieDetails = useSelector((store) => store.movieDetails);
+  const { id } = useParams();
+  console.log("movie details:", movieDetails);
+
+  //dispatch function to be called in useEffect
+  const getMovieDetails = () => {
+    dispatch({
+      type: "FETCH_MOVIE_DETAILS",
+      payload: id,
+    });
+  };
+
+  const handleEdit = () => {
+    history.push(`/edit/${id}`);
+  };
+
+  useEffect(() => {
+    getMovieDetails();
+  }, []);
+
+  return (
+    <>
+      {movieDetails.title ? (
+        <div className="movie-description">
+          <div className="movie-card">
+            <Card sx={{ maxWidth: 700, padding: 2 }}>
+              <CardContent>
+                <Typography sx={{ fontSize: 30 }} gutterBottom>
+                  {movieDetails.title}
+                </Typography>
+                <img
+                  className="poster"
+                  src={movieDetails.poster}
+                  alt={movieDetails.title}
+                />
+                <Typography sx={{ fontSize: 20 }} gutterBottom>
+                  {movieDetails.genres}
+                </Typography>
+                <Typography sx={{ fontSize: 16 }} gutterBottom>
+                  {movieDetails.description}
+                </Typography>
+                <Button onClick={handleEdit} variant="contained">
+                  Edit
+                </Button>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default MovieDetails;
